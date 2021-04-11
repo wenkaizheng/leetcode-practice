e545f85ae77a3d26b5a70c1a044b7f25da66f38a
wenkaizheng <wenkaizheng@email.arizona.edu>
2021-04-11 15:22:37 -0400

    update: rewrite some solutions

--- c++/first_and_last.cpp
+++ c++/first_and_last.cpp
@@ -3,29 +3,51 @@
 using namespace std;
 class Solution {
 public:
-  vector<int> searchRange(vector<int>& nums, int target) {
-    int start = 0, end = nums.size(), mid, left, right;
-    while (start < end) {
-        mid = (start + end) / 2;
-        if (nums[mid] >= target)
-            end = mid;
-        else
-            start = mid + 1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> rv{ -1,-1 };
+        int low_bound = search_range_helper(nums,target,0);
+        if (low_bound == -1){
+            return rv;
+        }
+        int high_bound = search_range_helper(nums,target,1);
+        return vector<int>{low_bound,high_bound};
     }
-    left = start;
-    end = nums.size();
-    while (start < end) {
-        mid = (start + end) / 2;
-        if (nums[mid] > target)
-            end = mid;
-        else
-            start = mid + 1;
+    int search_range_helper(vector<int>& nums, int target, int flag){
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if (nums[mid] == target){
+                if (flag){
+                    if (mid == high || nums[mid + 1] != target){
+                        return mid;
+                    }
+                    else{
+                        low = mid + 1;
+                    }
+                }
+
+                else{
+                    if (mid == low || nums[mid-1] != target){
+                        return mid;
+                    }
+                    else{
+                        high = mid -1;
+                    }
+                }
+
+            }
+            else if (nums[mid] > target){
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
-    right = start;
-    //cout << right << left << endl;
-    return left == right ? vector<int> {-1,-1} : vector<int> {left,right-1};
-}
-    
+
+
 };
 int main(){
     int a[9] = {-1,0,1,2,3,4,5,6,6};
--- python/rotated_sorted.py
+++ python/rotated_sorted.py
@@ -7,38 +7,58 @@ class Solution:
             mid = (start + end) // 2
             if target == nums[mid]:
                 return mid
-            if mid == start == end:
-                return -1
-            if nums[start] <= nums[mid]:
-                if nums[start] <= target < nums[mid]:
+            elif target == nums[start]:
+               return start
+            elif target == nums[end]:
+                return end
+            # [1,3]
+            if nums[start] < nums[mid]:
+                if nums[start] < target < nums[mid]:
                     end  = mid-1
                 else:
                     start = mid + 1
             else:
+                #assert(nums[start]!=nums[mid])
                 if nums[mid] < target < nums[start]:
                     start = mid + 1
                 else:
                     end = mid-1
         return -1
+        
     def search2(self,nums:List[int],target:int) -> int:
-        start = 0
-        end = len(nums)-1
-        while start <= end:
-            mid = (start + end) // 2
-            if nums[start] == target or nums[end] == target or nums[mid] == target:
-                return 1
-            if nums[start] < nums[mid]:
-                if nums[start] <= target < nums[mid]:
-                    end  = mid-1
+        if not nums: 
+            return 0
+        
+        low = 0
+        high = len(nums)-1 
+        #[0,0,1,2,2,5,6]
+        #[0,1,2,2,5,6,0]
+        while low<=high:
+
+            mid = (low+high)//2
+
+            if nums[mid] == target or nums[low]==target or nums[high]==target: 
+                return 1 
+            #find 3
+            # [1,1,3,1]
+            if nums[mid]<nums[low]:
+                if nums[mid]<target<nums[low]:
+                    low = mid+1
                 else:
-                    start = mid + 1
-            elif nums[start] > nums[mid]:
-                if nums[mid] < target < nums[start]:
-                    start = mid + 1
-                else:
-                    end = mid-1
+                    high = mid-1
+          
+            # [1,3,1,1,1]
+            elif nums[mid]>nums[low]:
+           # else:
+                 if nums[low] < target < nums[mid]:
+                     high = mid-1
+                 else:
+                    low = mid+1
+              
             else:
-                start = start + 1
+                  low += 1
+                 
+        
         return 0
 s = Solution()
 print(s.search([4,5,6,7,0,1,2],0))
