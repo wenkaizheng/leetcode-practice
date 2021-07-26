56eac60a3b897b2ce1c14f2a64e91535a02715bb
wenkaizheng <wenkaizheng@email.arizona.edu>
2021-07-26 19:39:43 -0400

    update: one time checking is enough

--- c++/sliding_window.cpp
+++ c++/sliding_window.cpp
@@ -72,7 +72,7 @@ public:
         for(int high = 0; high < s.length(); high++) {
             int current_letter_count = ++letters[s[high]];
             most_frequent_count = max(most_frequent_count, current_letter_count);
-            while(high-low+1 - most_frequent_count > k) {
+            if(high-low+1 - most_frequent_count > k) {
                 letters[s[low]]--;
                 low++;
             }
