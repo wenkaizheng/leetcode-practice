285c0fe18cd663fd66440b252a25698a62878082
wenkaizheng <wenkaizheng@email.arizona.edu>
2021-08-07 16:23:51 -0400

    update: dfs expression problem

---
+++ python/expression_add_operator.py
@@ -0,0 +1,55 @@
+class Solution:
+    def addOperators(self, num: 'str', target: 'int') -> 'List[str]':
+
+        N = len(num)
+        answers = []
+        def recurse(index, prev_operand, current_operand, value, string):
+
+            # Done processing all the digits in num
+            print(current_operand)
+            if index == N:
+
+                # If the final value == target expected AND
+                # no operand is left unprocessed
+                if value == target and current_operand == 0:
+                    answers.append("".join(string[1:]))
+                return
+
+            # Extending the current operand by one digit
+            current_operand = current_operand*10 + int(num[index])
+            str_op = str(current_operand)
+
+            # To avoid cases where we have 1 + 05 or 1 * 05 since 05 won't be a
+            # valid operand. Hence this check
+            if current_operand > 0:
+
+                # NO OP recursion
+                print("no op",prev_operand,current_operand,value,string)
+                recurse(index + 1, prev_operand, current_operand, value, string)
+
+            # ADDITION
+            string.append('+'); string.append(str_op)
+            print("add",prev_operand,current_operand,value,string)
+            recurse(index + 1, current_operand, 0, value + current_operand, string)
+            string.pop();string.pop()
+
+            # Can subtract or multiply only if there are some previous operands
+            if string:
+
+                # SUBTRACTION
+                string.append('-'); string.append(str_op)
+                print("sub",prev_operand,current_operand,value,string)
+                recurse(index + 1, -current_operand, 0, value - current_operand, string)
+                string.pop();string.pop()
+
+                # MULTIPLICATION
+                string.append('*'); string.append(str_op)
+               # print(value,prev_operand)
+                print("mul",prev_operand,current_operand,value,string)
+                recurse(index + 1, current_operand * prev_operand, 0, value - prev_operand + (current_operand * prev_operand), string)
+                string.pop();string.pop()
+        recurse(0, 0, 0, 0, [])    
+        return answers
+
+s = Solution()
+print(s.addOperators("232",8))
