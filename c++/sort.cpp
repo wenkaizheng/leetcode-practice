20a29a364dc3167a6d54b5b38ab8093c17176f9d
wenkaizheng <wenkaizheng@email.arizona.edu>
2021-08-08 00:03:55 -0400

    update: add two sorting review

---
+++ c++/sort.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+using namespace std;
+void swap(int &a, int &b) {     //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void merge(int *array, int l, int m, int r) {
+   int i, j, k, nl, nr;
+   //size of left and right sub-arrays
+   nl = m-l+1; nr = r-m;
+   int larr[nl], rarr[nr];
+   //fill left and right sub-arrays
+   for(i = 0; i<nl; i++)
+      larr[i] = array[l+i];
+   for(j = 0; j<nr; j++)
+      rarr[j] = array[m+1+j];
+   i = 0; j = 0; k = l;
+   //marge temp arrays to real array
+   while(i < nl && j<nr) {
+      if(larr[i] <= rarr[j]) {
+         array[k] = larr[i];
+         i++;
+      }else{
+         array[k] = rarr[j];
+         j++;
+      }
+      k++;
+   }
+   while(i<nl) {       //extra element in left array
+      array[k] = larr[i];
+      i++; k++;
+   }
+   while(j<nr) {     //extra element in right array
+      array[k] = rarr[j];
+      j++; k++;
+   }
+}
+void mergeSort(int *array, int l, int r) {
+
+   if(l < r) {
+      int m = (r+l)/2;
+      // Sort first and second arrays
+      mergeSort(array, l, m);
+      mergeSort(array, m+1, r);
+      merge(array, l, m, r);
+   }
+}
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high]; // pivot
+    int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot)
+        {
+            i++; // increment index of smaller element
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+        at right place */
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    //cout << "Enter the number of elements: ";
+    // cin >> n;
+    int arr[10] = {4,1,3,4,6,7,5,0,2,1};     //create an array with given number of elements
+
+    cout << "Array before merge Sorting: ";
+    display(arr, 10);
+    mergeSort(arr, 0, 9);     //(n-1) for last index
+    cout << "Array after merge Sorting: ";
+    display(arr, 10);
+
+    int arr1[10] = {4,1,3,4,6,7,5,0,2,1};     //create an array with given number of elements
+
+    cout << "Array before quick Sorting: ";
+    display(arr1, 10);
+    quickSort(arr1, 0, 9);     //(n-1) for last index
+    cout << "Array after qiick Sorting: ";
+    display(arr1, 10);
+}
