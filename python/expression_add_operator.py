533230a0f169a27953f4ecd59b1818431b55906e
wenkaizheng <wenkaizheng@email.arizona.edu>
2021-09-06 16:45:52 -0400

    update: expression add operator without merge two nearby integer

--- python/expression_add_operator.py
+++ python/expression_add_operator.py
@@ -51,5 +51,55 @@ class Solution:
         recurse(0, 0, 0, 0, [])    
         return answers
 
+class Solution1:
+    def addOperators(self, num: 'str', target: 'int') -> 'List[str]':
+
+        N = len(num)
+        answers = []
+        def recurse(index, prev_operand, value, string):
+
+            # Done processing all the digits in num
+            
+            if index == N:
+
+                # If the final value == target expected AND
+                # no operand is left unprocessed
+                if value == target:
+                    answers.append("".join(string[1:]))
+                return
+
+            # Extending the current operand by one digit
+            current_operand = int(num[index])
+            str_op = str(current_operand)
+
+        
+
+            # ADDITION
+            string.append('+'); string.append(str_op)
+            print("add",prev_operand,current_operand,value,string)
+            recurse(index + 1, current_operand, value + current_operand, string)
+            string.pop();string.pop()
+
+            # Can subtract or multiply only if there are some previous operands
+            if string:
+
+                # SUBTRACTION
+                string.append('-'); string.append(str_op)
+                print("sub",prev_operand,current_operand,value,string)
+                recurse(index + 1, -current_operand, value - current_operand, string)
+                string.pop();string.pop()
+
+                # MULTIPLICATION
+                string.append('*'); string.append(str_op)
+               # print(value,prev_operand)
+                print("mul",prev_operand,current_operand,value,string)
+                recurse(index + 1, current_operand * prev_operand, value - prev_operand + (current_operand * prev_operand), string)
+                string.pop();string.pop()
+        recurse(0, 0, 0, [])    
+        return answers
 s = Solution()
-print(s.addOperators("232",8))
+print(s.addOperators("1234",46))
+print(s.addOperators("1234",20))
+s = Solution1()
+print(s.addOperators("1234",46))
+print(s.addOperators("1234",10))
