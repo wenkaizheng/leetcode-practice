bfd01b3bdb986c4a65fbb8a5640221983358012d
wenkaizheng <wenkaizheng@email.arizona.edu>
2021-10-01 16:21:38 -0400

    update: add more sliding window problem

--- c++/sliding_window.cpp
+++ c++/sliding_window.cpp
@@ -106,6 +106,40 @@ public:
         return total+max_sum;
     }
 };
+class Solution5 {
+public:
+    int maxVowels(string s, int k) {
+
+        // Store current count of vowels in currCount
+        // and maximum currCount occurred in ans
+        int currCount = 0;
+        int ans = 0;
+
+        // An array to mark all the vowels' position to true
+        // for checking whether the char is vowel or not
+        bool vowels[26] = {false};
+        vowels[0] = vowels[4] = vowels[8] = vowels[14] = vowels[20] = true;
+
+        // Sliding window implementation
+        for (int i=0; i < s.length(); i++) {
+
+            // if any vowel is removed from left-> decrement
+            if (i >= k and vowels[s[i-k]-'a']) {
+                currCount -= 1;
+            }
+
+            // if any vowel is inserted from right-> increment
+            if (vowels[s[i]-'a']) {
+                currCount += 1;
+            }
+
+            // Store maximum occurence of currCount in ans
+            ans = max (ans, currCount);
+        }
+
+        return ans ;
+    }
+};
 
 int main(){
     vector<int> list2 {2,3,1,2,4,3};
@@ -132,6 +166,11 @@ int main(){
     vector<int> test{1,0,1,2,1,1,7,5};
     vector<int> test1{0,1,0,1,0,1,0,1};
     std::cout << s4.maxSatisfied(test,test1,3) << std::endl;
+
+    Solution5 s5;
+    string test0 = "abciiidef";
+    std::cout << s5.maxVowels(test0,3) << std::endl;
+
     return 0;
 
 }
